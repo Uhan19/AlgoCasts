40cb6ce9f78049ce51b44b87ac5ab2501ec9ca61
Yuehan Duan <Yuehan.Duan@walmartlabs.com>
2019-02-25 14:06:11 -0800

    Solve reverseString

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/*
+O - string
+I - string
+C - none
+E - none
+*/
+function reverse(str) {
+	// native methods
+		// using array.reverse()
+		// return str.split('').reverse().join('')
+	// not using .reverse();
+		// reverse loop through the str and concat it to new string
+	let reversedStr = '';
+		// old way of writing a for loop
+		// for (let i = str.length - 1; i >= 0; i--) {
+		// 	reversedStr = reversedStr + str[i];
+		// }
+	// ES6 for loop (not as flexible as old way)
+	for (let char of str) {
+		reversedStr = char + reversedStr;
+	}
+	return reversedStr;
+
+	// reduce solution
+	// return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
