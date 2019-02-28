3175245d03235033b4be15fcfee984e50c15ab57
Yuehan Duan <Yuehan.Duan@walmartlabs.com>
2019-02-27 17:16:06 -0800

    Solve reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,36 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+/*
+O - number
+I - number
+C - none
+E - negative number
+*/
+
+// function reverseInt(n) {
+//     let strNum = n + "";
+//     let reverseNum
+//     if(n >= 0) {
+//         reverseNum = strNum.split("").reduce((num, char) => char + num, "");
+//     }
+//     if(n < 0) {
+//         reverseNum = strNum.split("");
+//         reverseNum.shift()
+//         reverseNum.push("-")
+//         reverseNum = reverseNum.reduce((num, char) => char + num, "")
+//     }
+//     return reverseNum * 1;
+// }
+
+function reverseInt(n) {
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+
+    return parseInt(reversed) * Math.sign(n);
+  }
 
 module.exports = reverseInt;
